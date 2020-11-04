a6e5348827100f0b1b1332f110bb13edc7c0d74c
Brandon Jenniges <steveholt55@gmail.com>
2020-11-04 12:24:27 -0600

    Initial Commit

---
+++ CalculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by Brandon Jenniges on 11/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
